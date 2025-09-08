047475d4b6eb73ed80c2cbafd2b8deadcc4e5190
pedro <pedro311@gmail.com>
2025-09-08 14:59:12 +0200

    shared: defaults.c: add forgotten bwl_brX variables to BRIDGE_BLOCK_CORE

--- release/src-rt-6.x.4708/router/shared/defaults.c
+++ release/src-rt-6.x.4708/router/shared/defaults.c
@@ -292,7 +292,13 @@ struct nvram_tuple bsd_defaults[] = {
 	{ "dhcpd" #i "_startip",	"" 				, 0 }, \
 	{ "dhcpd" #i "_endip",		"" 				, 0 }, \
 	{ "dhcpd" #i "_ostatic",	"0"				, 0 }, /* ignore DHCP requests from unknown devices on LANX */ \
-	{ "dhcp" #i "_lease",		"1440"				, 0 },
+	{ "dhcp" #i "_lease",		"1440"				, 0 }, \
+	{ "bwl_br" #i "_enable",	"0"				, 0 }, \
+	{ "bwl_br" #i "_dlc",		""				, 0 }, \
+	{ "bwl_br" #i "_ulc",		""				, 0 }, \
+	{ "bwl_br" #i "_dlr",		""				, 0 }, \
+	{ "bwl_br" #i "_ulr",		""				, 0 }, \
+	{ "bwl_br" #i "_prio",		"2"				, 0 },
 #ifdef TCONFIG_OPENVPN
  #define BRIDGE_BLOCK_OPENVPN(i) \
 	{ "vpn_server1_plan" #i,	"0"				, 0 }, \
@@ -1794,24 +1800,6 @@ struct nvram_tuple router_defaults[] = {
 	{ "bwl_br0_tcp",		"0"				, 0 },	/* unlimited */
 	{ "bwl_br0_udp",		"0"				, 0 },	/* unlimited */
 	{ "bwl_br0_prio",		"3"				, 0 },
-	{ "bwl_br1_enable",		"0"				, 0 },
-	{ "bwl_br1_dlc",		""				, 0 },
-	{ "bwl_br1_ulc",		""				, 0 },
-	{ "bwl_br1_dlr",		""				, 0 },
-	{ "bwl_br1_ulr",		""				, 0 },
-	{ "bwl_br1_prio",		"2"				, 0 },
-	{ "bwl_br2_enable",		"0"				, 0 },
-	{ "bwl_br2_dlc",		""				, 0 },
-	{ "bwl_br2_ulc",		""				, 0 },
-	{ "bwl_br2_dlr",		""				, 0 },
-	{ "bwl_br2_ulr",		""				, 0 },
-	{ "bwl_br2_prio",		"2"				, 0 },
-	{ "bwl_br3_enable",		"0"				, 0 },
-	{ "bwl_br3_dlc",		""				, 0 },
-	{ "bwl_br3_ulc",		""				, 0 },
-	{ "bwl_br3_dlr",		""				, 0 },
-	{ "bwl_br3_ulr",		""				, 0 },
-	{ "bwl_br3_prio",		"2"				, 0 },
 
 #ifdef TCONFIG_NOCAT
 	{ "NC_enable",			"0"				, 0 },	/* enable NoCatSplash */
